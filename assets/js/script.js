8ed31b49bbe43a8a62736c98b8e6d1c7c25af905
jobemcamera <jobecamera@gmail.com>
2023-01-14 08:50:32 -0300

    async await / try catch

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,19 @@
 // Desta forma o .json() transforma o corpo e retorna um json formatado. 
 // O formato JSON (JavaScript Object Notation) possui basicamente a mesma sintaxe 
 // 000000000000000000000que a de um objeto JS.
+// try / catch para tratar o erro da promise
 
-var consultaCEP = fetch('http://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse CEP não existe!')
-        } else 
-            console.log(r)
-    })
-    .catch(erro => console.log(erro));
+async function buscaEndereco() {
+    try {
+        var consultaCEP = await fetch('http://viacep.com.br/ws/01001000/json/')
+        var consultaCEPConvertida = await consultaCEP.json()
+        if (consultaCEPConvertida.erro) { //retorna TRUE se houver erro
+            throw Error('CEP não existente!')
+        }
+        console.log(consultaCEPConvertida)
+    } catch (erro) {
+        console.log(erro)
+    }
+}
 
+buscaEndereco()
